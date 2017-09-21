4f9a84e67779a3d5d44fd71ee2903a15a62980f8
Clark Du <clark.duxin@gmail.com>
2017-09-21 15:18:36 +0800

    fix(tabs): [Vue warn]: Unknown custom element: <f7-navbar>,<f7-page>

--- templates/plugin.js
+++ templates/plugin.js
@@ -5,7 +5,7 @@ import Vue from 'vue'
 import Framework7 from 'framework7'
 
 // Import F7 Vue Plugin
-import { Framework7Vue } from 'framework7-vue'
+import { Framework7Vue, f7Navbar, f7Page } from 'framework7-vue'
 
 // Import F7 Theme Styles
 <% if (options.css) { %>
@@ -15,6 +15,9 @@ import 'framework7-css'
 // Init F7 Vue Plugin
 Vue.use(Framework7Vue, Framework7)
 
+Vue.component('f7-navbar', f7Navbar)
+Vue.component('f7-page', f7Page)
+  
 // Routes
 import routes from '~/f7/routes'
 
