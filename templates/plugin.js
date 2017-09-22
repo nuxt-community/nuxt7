2efe0c718f09485e6b967831e7c759da538ee673
Pooya Parsa <pooya@pi0.ir>
2017-09-22 10:18:34 +0330

    feat: experiments for framework7io/Framework7#1817

--- templates/plugin.js
+++ templates/plugin.js
@@ -7,11 +7,6 @@ import Framework7 from 'framework7'
 // Import F7 Vue Plugin
 import { Framework7Vue, f7Navbar, f7Page } from 'framework7-vue'
 
-// Import F7 Theme Styles
-<% if (options.css) { %>
-import 'framework7-css'
-<% } %>
-
 // Init F7 Vue Plugin
 Vue.use(Framework7Vue, Framework7)
 
@@ -20,6 +15,13 @@ Vue.component('f7-page', f7Page)
   
 // Routes
 import routes from '~/f7/routes'
+
+// Disable android contextmenu
+window.oncontextmenu = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+};
 
 // Register plugin
 export default async function framework7({ app }, inject) {
