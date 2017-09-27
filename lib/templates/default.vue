ccf2716847d53af6263f80178b18556a5ace3537
Pooya Parsa <pooya@pi0.ir>
2017-09-27 14:52:42 +0330

    feat: basic layout support

---
+++ lib/templates/default.vue
@@ -0,0 +1,19 @@
+<script>
+import { f7View, f7Statusbar } from 'framework7-vue'
+
+export default {
+  name: 'f7-layout',
+  components: {
+    f7View,
+    f7Statusbar
+  },
+  render (h) {
+    return h('div', {
+      class: "framework7-root"
+    }, [
+        h('f7-statusbar'),
+        h('nuxt')
+      ])
+  },
+}
+</script>
