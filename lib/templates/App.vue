1258abefa7141dfedb19c4158fdd27d5b9895166
Pooya Parsa <pooya@pi0.ir>
2017-11-08 00:02:44 +0330

    workaround for HMR problem
    Aalso prepares for upcoming internal nuxt refactors which App.vue is App.js

--- lib/templates/App.vue
+++ lib/templates/App.vue
@@ -1,42 +1,5 @@
 <script>
-import Vue from 'vue'
-
-// Styles
-<% css.forEach(function (c) { %>import '<%= relativeToBuild(resolvePath(c.src || c)) %>'
-<% }) %>
-
-// Default layout
-import _default from '<%= layouts['default'] %>'
-
-export default {
-  name: 'app',
-  functional: true,
-  render(h) {
-    return h(_default, {
-        class: 'framework7-root'
-    })
-  },
-  beforeCreate () {
-    Vue.util.defineReactive(this, 'nuxt', this.$options._nuxt)
-  },
-  created () {
-    // Add this.$nuxt in child instances
-    Vue.prototype.$nuxt = this
-    // Add to window so we can listen when ready
-    if (typeof window !== 'undefined') {
-      window.$nuxt = this
-    }
-    // Add $nuxt.error()
-    this.error = this.nuxt.error
-  },
-  methods: {
-    setLayout (layout) {
-      // STUB
-    },
-    loadLayout (layout) {
-      // STUB
-    }
-  },
-  head: <%= JSON.stringify(head) %>,
-}
+// Support for nuxt <= 1.0.0-rc11
+import App from './App.js'
+export default App
 </script>
