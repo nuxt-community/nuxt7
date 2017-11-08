034743f941aa5afa77de91f035f5f3a925fbd614
Pooya Parsa <pooya@pi0.ir>
2017-11-08 14:05:32 +0330

    prepare for nuxt 1.0.0

--- lib/templates/App.vue
+++ lib/templates/App.vue
@@ -1,5 +1,1 @@
-<script>
-// Support for nuxt <= 1.0.0-rc11
-import App from './App.js'
-export default App
-</script>
+<script src="./App.js"></script>
--- lib/templates/components/nuxt.vue
+++ lib/templates/components/nuxt.vue
@@ -1,16 +1,1 @@
-<script>
-import Vue from 'vue'
-
-export default {
-  name: 'nuxt',
-  functional: true,
-  beforeCreate () {
-    Vue.util.defineReactive(this, 'nuxt', this.$root.$options._nuxt)
-  },
-  render (h) {
-    return h('f7-view', {
-      props: <%= serialize(options.view).replace(/"__([^_]+)__"/g,"$1") %>
-    })
-  }
-}
-</script>
+<script src="./nuxt.js"></script>
