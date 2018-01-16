d99e05bde3739b41cd0d61332ed2f7b7782c7940
Pooya Parsa <pooya@pi0.ir>
2018-01-16 10:41:48 +0330

    add multi param example

---
+++ examples/with-params/pages/blog/_year/_id.vue
@@ -0,0 +1,22 @@
+<template>
+<f7-page>
+    <f7-navbar title="Blog" back-link="Back" />
+
+    <f7-block>
+      <h2>Year: {{ year }}</h2>
+      <h2>Id: {{ id }}</h2>
+    </f7-block>
+</f7-page>
+</template>
+
+<script>
+export default {
+  data () {
+    const { year, id } = this.$f7route.params
+    return {
+      year,
+      id
+    }
+  }
+}
+</script>
--- examples/with-params/pages/index.vue
+++ examples/with-params/pages/index.vue
@@ -1,10 +1,25 @@
 <template>
 <f7-page>
-    <f7-navbar title="Users List" />
+    <f7-navbar title="With params" />
+    <f7-block-title>Blog</f7-block-title>
+        <f7-list>
+        <template
+            v-for="y in [2017,2018]"
+        >
+            <f7-list-button
+                v-for="i in 5"
+                :key="i"
+                :href="`/blog/${y}/${i}`"
+            >
+                {{ y }} - {{ i }}
+            </f7-list-button>
+        </template>
+    </f7-list>
+
     <f7-block-title>Users</f7-block-title>
     <f7-list>
         <f7-list-button
-            v-for="i in 10"
+            v-for="i in 5"
             :key="i"
             :href="`/users/${i}`"
         >
