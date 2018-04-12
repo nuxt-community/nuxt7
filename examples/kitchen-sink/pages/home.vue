a30ffcf0f8258a8e079a6bedb21b9eeee63b638e
Pooya Parsa <pooya@pi0.ir>
2018-04-12 21:16:43 +0430

    misc(examples): sync kitchen-sink

--- examples/kitchen-sink/pages/home.vue
+++ examples/kitchen-sink/pages/home.vue
@@ -81,6 +81,9 @@
       <f7-list-item link="/list/" title="List View">
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
+      <f7-list-item link="/list-index/" title="List Index">
+        <f7-icon slot="media" icon="icon-f7"></f7-icon>
+      </f7-list-item>
       <f7-list-item link="/login-screen/" title="Login Screen">
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
@@ -139,6 +142,9 @@
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
       <f7-list-item link="/statusbar/" title="Statusbar">
+        <f7-icon slot="media" icon="icon-f7"></f7-icon>
+      </f7-list-item>
+      <f7-list-item link="/stepper/" title="Stepper">
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
       <f7-list-item link="/subnavbar/" title="Subnavbar">
