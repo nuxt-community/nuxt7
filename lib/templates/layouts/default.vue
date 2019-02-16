8e5ba4e478afdb0f51859db679d064c0b66c5460
pooya parsa <pooya@pi0.ir>
2019-02-16 18:47:54 +0330

    make it working

--- lib/templates/layouts/default.vue
+++ lib/templates/layouts/default.vue
@@ -1,15 +1,10 @@
 <script>
-import { f7View, f7Statusbar } from 'framework7-vue'
+import { f7Statusbar } from 'framework7-vue'
 
 export default {
-  name: 'f7-layout',
-  components: {
-    f7View,
-    f7Statusbar
-  },
   render (h) {
-    return h('div', [
-      h('f7-statusbar'),
+    return h('div',[
+      h(f7Statusbar),
       h('nuxt')
     ])
   },
