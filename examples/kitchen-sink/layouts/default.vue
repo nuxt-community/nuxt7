dae7933566e3dc6cee1842b45f2fb6773b40c542
pooya parsa <pooya@pi0.ir>
2019-02-17 12:58:20 +0330

    example: make panel working

---
+++ examples/kitchen-sink/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <f7-statusbar></f7-statusbar>
+    <f7-panel left cover>
+      <f7-view url="/panel-left/" links-view=".view-main"></f7-view>
+    </f7-panel>
+    <f7-panel right reveal>
+      <f7-view url="/panel-right/"></f7-view>
+    </f7-panel>
+    <nuxt></nuxt>
+  </div>
+</template>
